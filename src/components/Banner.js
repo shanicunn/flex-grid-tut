c1c533c9a28d823c43eb57e0b763ec3ad076843e
Shani <36285409+shanicunn@users.noreply.github.com>
2021-02-09 23:35:26 -0500

    Banner, Home, Nav, and Blog components created

---
+++ src/components/Banner.js
@@ -0,0 +1,8 @@
+function Banner() {
+    return (
+        <main id="Banner">
+            <h1>Frontend Shani</h1>
+        </main>
+    )
+}
+export default Banner;
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+function Nav() {
+    return (
+        <main id="Nav">
+            <ul>
+                <li>Home</li>
+                <li>Blogs</li>
+                <li>Resources</li>
+            </ul>
+        </main>
+    )
+}
+export default Nav;
