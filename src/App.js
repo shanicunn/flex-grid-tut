f172469afbc01727f0c234a0b889b77aa0474cab
Shani <36285409+shanicunn@users.noreply.github.com>
2021-02-09 23:36:45 -0500

    Made Banner visible

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from './components/Home';
 import Nav from './components/Nav';
+import Banner from './components/Banner';
 import './App.css';
 
 function App() {
   return (
     <main>
-      {/* <Banner /> */}
+      <Banner />
       <Nav />
       <Home />
     </main>
