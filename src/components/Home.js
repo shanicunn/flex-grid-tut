a01145f45fbf2a2675cb318236c9da1d11d83e03
Shani <36285409+shanicunn@users.noreply.github.com>
2021-02-10 00:19:29 -0500

    Edited typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
             <h2>What <i>are</i> Flexbox and CSS Grid?!</h2>
             <p>In layman's terms Flexbox allows you to put objects in columns and rows while also giving the flexibility to do things like shrink down or expand to fill spaces.</p>
             <p>CSS Grid on the other hand is great for creating page structure using columns and rows as well but without the movement you will see in Flexbox.</p>
-            <p>Sometimes the similarities can be confusing thus making it difficult to use them seperately or in conjunction. My plan is to help you feel more confident when using either especially if you were having some difficulty before!</p>
+            <p>Sometimes the similarities can be confusing thus making it difficult to use them seperately or in conjunction. My plan is to help you feel more confident when using either of them especially if you were having some difficulty before!</p>
         </main>
     )
 }
