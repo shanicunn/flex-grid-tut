6c59d64c53cef8dd4c460bfc91dc7c0d0da04284
Shani <36285409+shanicunn@users.noreply.github.com>
2021-02-10 00:20:09 -0500

    React router DOM utilized

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,47 @@
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import Home from './Home';
+import BlogMain from './BlogMain';
+import Resources from './Resources';
+
 function Nav() {
     return (
+        
+    <Router>
         <main id="Nav">
-            <ul>
-                <li>Home</li>
-                <li>Blogs</li>
-                <li>Resources</li>
-            </ul>
+        <nav>
+          <ul>
+            <li>
+            <Link className="Links" to="/home">Home</Link>
+            </li>
+            <li>
+            <Link className="Links" to="/blogs">Blogs</Link>
+            </li>
+            <li>
+            <Link className="Links" to="/resources">Resources</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/blogs">
+            <BlogMain />
+          </Route>
+          <Route path="/resources">
+            <Resources />
+          </Route>
+        </Switch>
         </main>
+    </Router>       
+         
+        
     )
 }
 export default Nav;
