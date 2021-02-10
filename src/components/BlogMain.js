e8b07e5e036756e9a625f4bbe12acd002c4e7891
Shani <36285409+shanicunn@users.noreply.github.com>
2021-02-10 00:19:03 -0500

    JSX structured for future layout

---
+++ src/components/BlogMain.js
@@ -0,0 +1,13 @@
+import Banner from './Banner';
+import Nav from './Nav';
+
+function BlogMain() {
+    return(
+        <main>
+            <Banner />
+            <Nav /> 
+            console.log("Blog page clicked");
+        </main>
+    )
+}
+export default BlogMain;
